3e28da384c1d2d8a03dfd9f58d4891cc14f827b9
tinajoelly <tinaandrea.joelly2@edu.fh-kaernten.ac.at>
2025-09-03 21:30:13 +0200

    first try at models

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('auth_app.api.urls', 'kanban_app.api.urls')),
+    path('api/', include('auth_app.api.urls')),
+    path('api/', include('kanban_app.api.urls')),
 ]
--- core/kanban_app/models.py
+++ core/kanban_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=255)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='owned_boards')
+    members = models.ManyToManyField(User, related_name='boards')
+
+
+class BoardTask(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=50)
+    priority = models.CharField(max_length=50)
+    duedate = models.DateField(auto_now_add=True)
+    assignee = models.OneToOneField(User, on_delete=models.CASCADE, related_name='assigned_task')
+    reviewer = models.OneToOneField(User, on_delete=models.CASCADE, related_name='reviewed_task')
+
+class TaskComment(models.Model):
+    task = models.ForeignKey(BoardTask, on_delete=models.CASCADE, related_name='comments')
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    content = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
